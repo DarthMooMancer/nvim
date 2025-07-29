61cc022845eba09d6021de0fed3dbe0a5a78107d
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-07-29 18:32:48 -0400

    wip

--- init.lua
+++ init.lua
@@ -1,56 +1,30 @@
-vim.opt.mouse =  ""
-vim.opt.completeopt = "noselect"
-vim.opt.termguicolors = true
-vim.opt.wrap = false
-vim.opt.relativenumber = true
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.signcolumn = "yes"
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.hlsearch = false
-vim.opt.swapfile = false
-vim.opt.clipboard = unnamedplus
+vim.opt.completeopt = { "menuone", "noselect", "popup" }
+-- vim.opt.winborder = 'rounded'
 
-vim.g.mapleader = " "
+vim.lsp.enable({ "lua_ls", "clangd" })
+vim.cmd.colorscheme('everforest')
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>ff", "gg=G")
-vim.keymap.set('n', '<leader><leader>', ":Telescope find_files<CR>", { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>xg', ":Telescope live_grep<CR>", { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>xh', ":Telescope help_tags<CR>", { desc = 'Telescope help tags' })
-vim.keymap.set("n", "<leader>xc", "<Cmd>cclose<CR>")
-vim.keymap.set("n", "<leader>xx", function()
-  vim.diagnostic.setqflist({ open = true })
-end, { desc = "Show diagnostics in quickfix" })
-
-vim.cmd.colorscheme('everforest')
-vim.lsp.enable({ "lua_ls", "clangd" })
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "qf",
-	callback = function()
-		vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR>:cclose<CR>", { noremap = true, silent = true })
-	end,
-})
-
+vim.keymap.set("n", "<leader>ff", "ggVG=")
+vim.keymap.set('n', '<leader><leader>', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>xh', ":Pick help<CR>")
 
 vim.pack.add({
-	"neovim/nvim-lspconfig",
-	"nvim-treesitter/nvim-treesitter",
 	"cohama/lexima.vim",
-	"DarthMoomancer/Polydev",
-	"nvim-telescope/telescope.nvim",
-	"nvim-lua/plenary.nvim",
+	{ src = 'echasnovski/mini.pick', version = '*' },
 	{ src = "saghen/blink.cmp", version = "v1.*" },
 })
 
-require("blink.cmp").setup({
-	completion = {
-		ghost_text = {
-			enabled = true
-		}
-	}
-})
-require("nvim-treesitter.configs").setup({
-	highlight = { enable = true },
-	indent = { enable = true }
-})
+require('mini.pick').setup()
+require("blink.cmp").setup()
